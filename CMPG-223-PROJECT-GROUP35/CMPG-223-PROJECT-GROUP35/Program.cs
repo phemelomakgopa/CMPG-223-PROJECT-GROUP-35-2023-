a524927c02c7873add91ab0adba876ef60b774fc
Phemelo Shaun Makgopa <phemeloshaunmakgopa@gmail.com>
2023-08-19 20:45:46 +0200

    Created the vs project

---
+++ CMPG-223-PROJECT-GROUP35/CMPG-223-PROJECT-GROUP35/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMPG_223_PROJECT_GROUP35
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
